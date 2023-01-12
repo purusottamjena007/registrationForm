46153d1907ca848f98a435245c39ebbbb5635196
purusottamjena007 <purusottamjena007@gmail.com>
2023-01-12 12:56:18 +0000

    registrationForm

---
+++ src/App.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-named-as-default-member */
+// eslint-disable-next-line import/no-named-as-default
+import RegistrationForm from './components/RegistrationForm'
+
+import './App.css'
+
+const App = () => <RegistrationForm />
+
+export default App
